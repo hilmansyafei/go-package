e814ebed1394e8d0c6f8c62ee19e6d30e26c7002
Nur Aizat Insan F <nuraizatif@gmail.com>
2019-06-28 18:46:39 +0700

    Add comment in func GenerateMockServer

--- modules/mockServer.go
+++ modules/mockServer.go
@@ -9,15 +9,19 @@ import (
 // GenerateMockServer is function to generate mock server with,
 // Custom statusCode, headers, and body.
 func GenerateMockServer(statusCode int, headersParam interface{}, body interface{}) *httptest.Server {
+	// Generate mock server.
 	mockserver := httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Set Http status code.
 			w.WriteHeader(statusCode)
+			// Set headers if any.
 			if headersParam != "" {
 				headers := headersParam.(map[string]string)
 				for k, v := range headers {
 					w.Header().Set(k, v)
 				}
 			}
+			// Generate body json.
 			json.NewEncoder(w).Encode(body)
 		}))
 	return mockserver
