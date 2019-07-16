e549cdd9ecd4190c6d5905a87e44642284f99ef1
Hilman <hilman@Hilmans-MacBook-Air.local>
2019-07-16 14:20:36 +0700

    Add mock function

--- database/mongo/mongo.go
+++ database/mongo/mongo.go
@@ -1,6 +1,7 @@
 package mongo
 
 import (
+	"errors"
 	"log"
 
 	"gopkg.in/mgo.v2"
@@ -16,8 +17,8 @@ type BaseStruct struct {
 
 // Mongo : hold global variabel
 type Mongo struct {
-	DB        *mgo.Database
-	Collectio *mgo.Collection
+	DB         *mgo.Database
+	Collection *mgo.Collection
 }
 
 // Configuration : hold data config database
@@ -36,6 +37,13 @@ type PagingQuery struct {
 	Offset int
 	Limit  int
 	Sort   string
+}
+
+// MongoMock : hold global variabel
+type MongoMock struct {
+	InterfaceReturn      interface{}
+	InterfaceReturnArray []interface{}
+	Data                 string
 }
 
 // MongoProvider : interface
@@ -123,4 +131,76 @@ func (m *Mongo) GetOne(collection string, query bson.M, mdl *interface{}) error 
 func (m *Mongo) Find(collection string, query bson.M, mdl *[]interface{}, pagingQuery PagingQuery) error {
 	Col := m.DB.C(collection)
 	return Col.Find(nil).Sort(pagingQuery.Sort).Skip(pagingQuery.Offset).Limit(pagingQuery.Limit).All(mdl)
+}
+
+// ------------------- MOCK ------------------ //
+
+// Create : Insert new record
+func (m *MongoMock) Create(collection string, update interface{}, mdl interface{}) (bson.ObjectId, *mgo.ChangeInfo, error) {
+	return "", nil, nil
+}
+
+// Delete : delete collection
+func (m *MongoMock) Delete(collection string, query bson.M) error {
+	return nil
+}
+
+// DeleteID : delete collection
+func (m *MongoMock) DeleteID(collection string, ID bson.ObjectId) error {
+	return nil
+}
+
+// DeleteAll : delete collection
+func (m *MongoMock) DeleteAll(collection string, query bson.M) (*mgo.ChangeInfo, error) {
+	return nil, nil
+}
+
+// Update all fields
+func (m *MongoMock) Update(collection string, query bson.M, update interface{}) error {
+	return nil
+}
+
+// GetAll record with primary key, return unique result
+func (m *MongoMock) GetAll(collection string, mdl *[]interface{}) error {
+	*mdl = m.InterfaceReturnArray
+	if m.InterfaceReturnArray == nil {
+		return errors.New("error")
+	}
+	return nil
+}
+
+// GetByID : mock function
+func (m *MongoMock) GetByID(collection string, ID bson.ObjectId, mdl *interface{}) error {
+	*mdl = m.InterfaceReturn
+	if m.InterfaceReturn == nil {
+		return errors.New("error")
+	}
+	return nil
+}
+
+// Get : Get list record
+func (m *MongoMock) Get(collection string, query bson.M, mdl *[]interface{}) error {
+	*mdl = m.InterfaceReturnArray
+	if m.InterfaceReturnArray == nil {
+		return errors.New("error")
+	}
+	return nil
+}
+
+// GetOne : Get one record
+func (m *MongoMock) GetOne(collection string, query bson.M, mdl *interface{}) error {
+	*mdl = m.InterfaceReturn
+	if m.InterfaceReturn == nil {
+		return errors.New("error")
+	}
+	return nil
+}
+
+// Find : Get records with paging query
+func (m *MongoMock) Find(collection string, query bson.M, mdl *[]interface{}, pagingQuery PagingQuery) error {
+	*mdl = m.InterfaceReturnArray
+	if m.InterfaceReturnArray == nil {
+		return errors.New("error")
+	}
+	return nil
 }
