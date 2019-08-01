929e0a4f9521292dcc5e9368ecf2cc235af07546
Hilman <hilman@Hilmans-MacBook-Air.local>
2019-08-01 16:04:23 +0700

    fix GenReqWithHeader

--- modules/client.go
+++ modules/client.go
@@ -1,7 +1,8 @@
 package modules
 
 import (
-	"io"
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"time"
 )
@@ -17,12 +18,19 @@ func GenClient(timeLimit time.Duration) http.Client {
 }
 
 // GenReqWithHeader : Generate request with header
-func GenReqWithHeader(method string, url string, headers map[string]string, timeLimit time.Duration, body io.Reader) (*http.Response, error) {
+func GenReqWithHeader(method string, url string, headers map[string]string, timeLimit time.Duration, body map[string]interface{}) (*http.Response, error) {
 	timeout := time.Duration(timeLimit * time.Second)
 	client := http.Client{
 		Timeout: timeout,
 	}
-	req, err := http.NewRequest(method, url, body)
+	bodyMarshal, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(bodyMarshal))
+	if err != nil {
+		return nil, err
+	}
 	for key, value := range headers {
 		req.Header.Add(key, value)
 	}
